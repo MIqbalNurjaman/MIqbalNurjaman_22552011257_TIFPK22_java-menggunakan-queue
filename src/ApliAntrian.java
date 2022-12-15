a01efffd0c939427998d2b27624c4e0ac40bb681
M. Iqbal Nurjaman <iqbalnurjaman298@gmail.com>
2022-12-16 00:12:06 +0700

    first commit

---
+++ src/ApliAntrian.java
@@ -0,0 +1,16 @@
+public class ApliAntrian {
+
+    public static void main(String[] args){
+        Antrian antrian = new Antrian(10);
+        antrian.masuk(13);
+        antrian.lihat();
+        antrian.masuk(32);
+        antrian.lihat();
+        antrian.masuk(45);
+        antrian.lihat();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
+    }
+}
